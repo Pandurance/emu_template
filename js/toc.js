95d3eb88d9990370686731fb2b34fb073ccfe06b
Andy Zhang <z-c-ge@outlook.com>
2025-05-14 17:53:30 +0800

    Allow chapter numbering

--- js/toc.js
+++ js/toc.js
@@ -1,7 +1,19 @@
+function colToLetter(number) {
+    let result = '';
+    // number = number - 1; // If starting from 1
+    do {
+        const letter = String.fromCharCode(65 + (number % 26));
+        result = letter + result;
+        number = Math.floor(number / 26) - 1;
+    } while (number >= 0);
+    return result;
+}
+
 function createToc(pages) {
     let tocContent = $(`<div>
         <h2><span class="header">Table of contents</span></h2>
     </div>`);
+    let chapterCount = 0;
     for (let pageNumber = 0; pageNumber < pages.length; pageNumber++) {
         let i = pages[pageNumber];
         if (pageNumber === 0) continue;
@@ -10,17 +22,17 @@ function createToc(pages) {
         let mainArea = element.children("div.pagedjs_area");
         let pagedContentArea = mainArea.children("div.pagedjs_page_content").children("div");
         let chapterTitle = pagedContentArea.find("span.emu-chapter-name");
-        if (!chapterTitle.length) continue; // We're on a page without a title
-
         let bodyArea = pagedContentArea.children("div.emu-body-container");
-
-        tocContent.append(`
+        if (chapterTitle.length) {
+            tocContent.append(`
         <div class="emu-toc-row">
-            <strong>${chapterTitle.text()}</strong>
+            <strong>${colToLetter(chapterCount)} ${chapterTitle.text()}</strong>
             <span class="emu-toc-sep"></span>
-           <a href="#page-${pageNumber + 1}">${pageNumber + 1}</a>
+            <a href="#page-${pageNumber + 1}">${pageNumber + 1}</a>
         </div>
         `);
+            chapterCount++;
+        }
 
         // Go into the body area
         for (let bodyNode of bodyArea.find("span.header-text")) {
